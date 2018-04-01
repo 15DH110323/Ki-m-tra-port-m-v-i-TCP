3783c51a60d72f3ab7e86656f2973d6235bd120b
15DH110323 <vtanh26081997@gmail.com>
2018-04-01 21:20:02 +0700

    tcp

---
+++ CheckportWithTCP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckportWithTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipSever = new IPEndPoint(IPAddress.Parse("125.253.118.38"),8330);
+            TcpClient c = new TcpClient();
+            c.SendTimeout = 1000;
+            c.ReceiveTimeout = 1000;
+            try
+            {
+                c.Connect(ipSever);
+                Console.WriteLine(ipSever.ToString() + "is opened");
+                c.Close();
+
+            }
+            catch
+            {
+                Console.WriteLine(ipSever.ToString() + "is closed");
+            }
+        }
+    }
+}
